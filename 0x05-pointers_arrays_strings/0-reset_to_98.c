dc41de408621a40465caf0e3552ea3d1ac2874c2
aisha <Aishasani347@gmail.com>
2023-09-19 13:39:07 -0700

    projects on strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of n to 98
+ * @n: pointer to the input number n
+ */
+void reset_to_98(int *n)
+{
+    if (n != NULL)
+    {
+        *n = 98;
+    }
+}
